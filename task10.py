ecd3374c65fdbfc1309a710ba30bb1c0223b2844
taalaybolotbekov <bolotbekovtaalay@gmail.com>
2020-03-10 13:05:22 +0600

    ten commit

---
+++ task10.py
@@ -0,0 +1,18 @@
+f = 'In the first line, print the third character of this string'
+print(f[3])
+s = 'In the second line, print the second to last character of this string.'
+print(s[1:-1])
+t = 'In the third line, print the first five characters of this string.'
+print(t[0:5])
+f = 'In the fourth line, print all but the last two characters of this string.'
+print(f[0:-2])
+five = 'In the fifth line, print all the characters of this string with even indices.'
+print(five[1::2])
+six = 'In the sixth line, print all the characters of this string with odd indices.'
+print(six[0::2])
+sev = 'In the seventh line, print all the characters of the string in reverse order.'
+print(sev[::-1])
+ei = 'In the eighth line, print every second character of the string in reverse order,starting from the last one.'
+print(ei [::-2])
+nin = 'In the ninth line, print the length of the given string.'
+print(len(nin))
